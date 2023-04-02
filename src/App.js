8c812ac6021bda86e17207cb7200fbabcc97d616
Nikhil Raut <johndoe@example.com>
2023-04-02 17:51:43 +0530

    Done some modification and error handling

--- src/App.js
+++ src/App.js
@@ -4,24 +4,31 @@ import PlayerCard from './PlayerCard';
 
 import Axois from 'axios';
 
+let backupResult = []
+
 function App() {
 
   const [result, setResult] = useState([])
   const search = useRef(null)
 
   const handleSearch = () => {
-    let pattern = new RegExp(search.current.value, 'i')
-    console.log(pattern)
-    let newResult = []
-    Data.playerList.forEach(
-      ele => {
-        if (pattern.test(ele.PFName) || pattern.test(ele.TName)) {
-          newResult.push(ele)
+    try {
+      let pattern = new RegExp(search.current.value, 'i')
+      let newResult = []
+      backupResult.forEach(
+        ele => {
+          if (pattern.test(ele.PFName) || pattern.test(ele.TName)) {
+            newResult.push(ele)
+          }
         }
-      }
-    )
-    newResult = newResult.sort((a, b) => a.Value - b.Value)
-    setResult(newResult)
+      )
+      newResult = newResult.sort((a, b) => a.Value - b.Value)
+      setResult(newResult)
+    }
+    catch (e) {
+      console.log(e.message)
+      setResult([])
+    }
   }
 
   useEffect(
@@ -30,10 +37,12 @@ function App() {
         (res) => {
           console.log("Page is loaded.")
           let sortedResult = res.data.playerList.sort((a, b) => a.Value - b.Value)
+          backupResult = sortedResult
+          console.log(backupResult)
           setResult(sortedResult)
         }
       ).catch(
-        (err)=> {console.log(err)}
+        (err) => { console.log(err) }
       )
     }
     , [])
@@ -41,15 +50,19 @@ function App() {
   return (
     <div className="App">
       <div className='player-search'>
-        <input type='text' className='search' onChange={() => handleSearch()} placeholder='Search your player...' ref={search} />
+        <input type='search' className='search' onChange={() => handleSearch()} placeholder='Search your player...' ref={search} />
       </div>
-      <div className='player-result'>
-        {
-          result.map(
+
+      {
+        result.length === 0 ?
+          <div className='alert-msg'>No result found</div>
+          :
+          <div className='player-result'>
+            {result.map(
             (playerData, index) => <PlayerCard playerData={playerData} key={index} />
-          )
-        }
-      </div>
+            )}
+          </div>
+      }
     </div>
   );
 }
